3235b22e330b930805b9f4180e7848a56161f7eb
Matt Catellier <mattcatellier@gmail.com>
2019-09-30 15:12:47 -0700

    Fixed proxy issue, first api call with useEffect

--- client/src/PrivateLayout.js
+++ client/src/PrivateLayout.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Routes from './Routes';
 
 import './PrivateLayout.css';
 
-const PrivateLayout = () => 
-  <div className='layout'>
-    <div>Div 1</div>
-    <div><Routes /></div>
-  </div>
+
+
+const PrivateLayout = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const getExamples = async () => {
+      const result = (await axios.get("/api/example/")).data
+      console.log(result)
+      setData(result)
+    }
+    getExamples()
+  }, [])
+  return (
+    <div className='layout'>
+      <div>Div 1</div>
+      <div><Routes /></div>
+      { data.map(d => <div> {d.foo} </div>)}
+    </div>
+  )
+}
 
 export default PrivateLayout;
